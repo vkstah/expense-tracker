005c0eaecd65905880bc78f3725794c4a384bc9e
Vili Ståhlberg <vkstah@utu.fi>
2021-09-05 18:36:56 +0300

    Started working on the JavaScript-side of things.

---
+++ assets/scripts/main.js
@@ -0,0 +1,7 @@
+const balance = document.getElementById('balance');
+const form = document.getElementById('form');
+
+// Function to add a new transaction to the transaction list. This function also takes care of adding to either the total income or expense counter.
+function addTransaction(e) {
+    e.preventDefault();
+}
